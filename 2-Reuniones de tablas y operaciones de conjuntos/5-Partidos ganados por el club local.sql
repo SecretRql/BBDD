86870dd7dda2632f9169480c2bc552fa33416e1a
rql <r.pino.camacho@gmail.com>
2021-10-12 09:17:16 +0200

    Mejor solucion en problema 5

--- 2-Reuniones de tablas y operaciones de conjuntos/5-Partidos ganados por el club local.sql
+++ 2-Reuniones de tablas y operaciones de conjuntos/5-Partidos ganados por el club local.sql
@@ -1,18 +1,19 @@
 -- Escribe una consulta SQL tal que por cada partido ganado por el club local, muestre el nombre de los clubes enfrentados y el resultado. Los resultados deben aparecer ordenados de más a menos goles locales, en caso de igualdad de más a menos goles visitantes, y en caso de resultados iguales deben aparecer por nombre de club local descendente. El esquema esperado es:
 -- (ClubLocal, GolesLocal, GolesVisitante, ClubVisitante)
+
+
 SELECT
-    cl.nombre          AS "ClubLocal",
-    e.goleslocal       AS "GolesLocal",
-    e.golesvisitante   AS "GolesVisitante",
-    cv.nombre          AS "ClubVisitante"
+    l.nombre,
+    e.goleslocal,
+    e.golesvisitante,
+    v.nombre
 FROM
-    club       cl
-    INNER JOIN club       cv ON cl.nombre <> cv.nombre
-    INNER JOIN enfrenta   e ON ( e.cif_local = cl.cif
-                               AND e.cif_visitante = cv.cif )
+    enfrenta   e
+    INNER JOIN club       l ON (e.cif_local = l.cif)
+    INNER JOIN club       v ON (e.cif_visitante = v.cif)
 WHERE
-    e.goleslocal > e.golesvisitante
+    e.golesvisitante < e.goleslocal;
 ORDER BY
     e.goleslocal DESC,
     e.golesvisitante DESC,
-    cl.nombre DESC;
+    l.nombre DEC;

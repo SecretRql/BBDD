84f116b6bd79ffbacbcaf779a9800440a4ff60a4
Rql <74860846+SecretRql@users.noreply.github.com>
2021-10-25 12:40:03 +0200

    Create 2-Clubes sin jugadores.sql

---
+++ 4-Consultas Anidadas/2-Clubes sin jugadores.sql
@@ -0,0 +1,13 @@
+--Escribe una consulta que obtenga el CIF y el nombre de los clubes que no tienen ningún jugador. El esquema esperado es:
+--(CIF, Nombre)
+SELECT
+    c.cif      cif,
+    c.nombre   nombre
+FROM
+    club      c
+    LEFT JOIN jugador   j ON j.cif = c.cif
+WHERE
+    j.cif IS NULL
+GROUP BY
+    c.cif,
+    c.nombre
